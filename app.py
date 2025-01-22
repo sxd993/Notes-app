088bb6687259b0c2f3a61aa1d4c05d1bbeddeded
sxd993 <gamhotik2005@mail.ru>
2025-01-22 18:59:29 +0500

    added notes from sqlite3

--- app.py
+++ app.py
@@ -1,37 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask.views import MethodView
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'notes.db'
 
-class HomePage(MethodView):
-    def get(self):
-        return render_template('home.html')
+# Функция для работы с базой данных
+def execute_query(query, params=(), fetch=False):
+    with sqlite3.connect(DATABASE) as conn:
+        cur = conn.cursor()
+        cur.execute(query, params)
+        if fetch:
+            return cur.fetchall()
+        conn.commit()
 
-
-class NotePages(MethodView):
-    notes = []
-
-    def get(self):
-        return render_template('notes.html', notes=self.notes)
-    
-    def post(self):
+# Главная страница заметок
+@app.route('/', methods=['GET', 'POST'])
+def notes_page():
+    if request.method == 'POST':
         note = request.form.get('note')
         note_to_delete = request.form.get('note_to_delete')
 
         if note:
-            self.notes.append(note)
+            execute_query('INSERT INTO notes (content) VALUES (?)', (note,))
         elif note_to_delete:
-            if note_to_delete in self.notes:
-                self.notes.remove(note_to_delete)
-        
-        return redirect(url_for('notes'))
+            execute_query('DELETE FROM notes WHERE id = ?', (note_to_delete,))
 
+        return redirect(url_for('notes_page'))
 
+    notes = execute_query('SELECT id, content FROM notes', fetch=True)
+    return render_template('notes.html', notes=notes)
 
-app.add_url_rule('/ddd', view_func=HomePage.as_view('home'))    
-app.add_url_rule('/', view_func=NotePages.as_view('notes'))
-
-
-
+# Инициализация базы перед запуском приложения
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
--- database.py
+++ database.py
@@ -1,3 +1,14 @@
 import sqlite3
 
-db = 'notes.db'
+conn = sqlite3.connect('/Users/vladimir/Python/NotesApp/notes.db')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT NOT NULL
+            )'''
+)
+
+conn.commit()
+cur.close()
+conn.close()
